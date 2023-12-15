2a978bbbbf8a2759b7c570cdf0c6925f42ceab6d
OzodbekMahmaraimov <ozodbekmahmaraimov07@gmail.com>
2023-12-15 11:39:42 +0500

    portfolio

---
+++ js/script.js
@@ -0,0 +1,68 @@
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.onclick = function () {
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+}
+
+window.addEventListener('scroll', () => {
+    const backToTopBtn = document.getElementById('backToTopBtn');
+    if (window.scrollY > 150 && backToTopBtn) {
+        backToTopBtn.classList.add('show');
+        backToTopBtn.classList.remove('hide');
+
+    } else {
+        if (backToTopBtn) {
+            backToTopBtn.classList.remove('show');
+            backToTopBtn.classList.add('hide');
+        }
+    }
+});
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+// colors
+let home = document.querySelector('.one-page');
+let about = document.querySelector('.about-container');
+let myProject = document.querySelector('.my-project-container');
+let portfolio = document.querySelector('.portfolio');
+let contactUs = document.querySelector('.contact-body');
+
+// home 
+document.querySelector('#color-home').addEventListener('input', function() {
+    home.style.backgroundColor = document.querySelector('#color-home').value;
+})
+
+// about
+document.querySelector('#color-about').addEventListener('input', function() {
+    about.style.backgroundColor = document.querySelector('#color-about').value;
+})
+
+// my project
+document.querySelector('#color-project').addEventListener('input', function() {
+    myProject.style.background = document.querySelector('#color-project').value;
+})
+
+// portfolio
+document.querySelector('#color-portfolio').addEventListener('input', function() {
+    portfolio.style.backgroundColor = document.querySelector('#color-portfolio').value;
+})
+
+// contact US 
+document.querySelector('#color-contact').addEventListener('input', function() {
+    contactUs.style.background = document.querySelector('#color-contact').value;
+})
+
+const clearAll = () => {
+    home.style.backgroundColor = '#003049';
+    about.style.backgroundColor = '#040404';
+    myProject.style.background = 'linear-gradient(45deg, #08001f, #30197d)';
+    portfolio.style.backgroundColor = '#003049';
+    contactUs.style.background = '#2c2f48';
+    document.querySelector('#color-home').value = '';
+    document.querySelector('#color-about').value = '';
+    document.querySelector('#color-project').value = '';
+    document.querySelector('#color-portfolio').value = '';
+    document.querySelector('#color-contact').value = '';
+}
