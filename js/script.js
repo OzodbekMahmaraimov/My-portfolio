24262795aa430f053fef70b326579cffea7b921a
OzodbekMahmaraimov <ozodebkmahmaraimov07@gmail.com>
2024-01-06 23:56:00 +0500

    Avvalgidanda qulayroq

--- js/script.js
+++ js/script.js
@@ -1,83 +1,85 @@
-const hamburger = document.querySelector(".hamburger");
+window.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector(".hamburger");
 
-hamburger.onclick = function () {
-    navBar = document.querySelector(".nav-bar");
-    navBar.classList.toggle("active");
-}
-
-window.addEventListener('scroll', () => {
-    const backToTopBtn = document.getElementById('backToTopBtn');
-    if (window.scrollY > 150 && backToTopBtn) {
-        backToTopBtn.classList.add('show');
-        backToTopBtn.classList.remove('hide');
-
-    } else {
-        if (backToTopBtn) {
-            backToTopBtn.classList.remove('show');
-            backToTopBtn.classList.add('hide');
-        }
+    hamburger.onclick = function () {
+        navBar = document.querySelector(".nav-bar");
+        navBar.classList.toggle("active");
     }
-});
 
-function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-}
-// colors
-let home = document.querySelector('.one-page');
-let about = document.querySelector('.about-container');
-let myProject = document.querySelector('.my-project-container');
-let portfolio = document.querySelector('.portfolio');
-let contactUs = document.querySelector('.contact-body');
+    window.addEventListener('scroll', () => {
+        const backToTopBtn = document.getElementById('backToTopBtn');
+        if (window.scrollY > 150 && backToTopBtn) {
+            backToTopBtn.classList.add('show');
+            backToTopBtn.classList.remove('hide');
 
-// home 
-document.querySelector('#color-home').addEventListener('input', function () {
-    home.style.backgroundColor = document.querySelector('#color-home').value;
-})
+        } else {
+            if (backToTopBtn) {
+                backToTopBtn.classList.remove('show');
+                backToTopBtn.classList.add('hide');
+            }
+        }
+    });
 
-// about
-document.querySelector('#color-about').addEventListener('input', function () {
-    about.style.backgroundColor = document.querySelector('#color-about').value;
-})
+    function scrollToTop() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+    // colors
+    let home = document.querySelector('.one-page');
+    let about = document.querySelector('.about-container');
+    let myProject = document.querySelector('.my-project-container');
+    let portfolio = document.querySelector('.portfolio');
+    let contactUs = document.querySelector('.contact-body');
 
-// my project
-document.querySelector('#color-project').addEventListener('input', function () {
-    myProject.style.background = document.querySelector('#color-project').value;
-})
+    // home 
+    document.querySelector('#color-home').addEventListener('input', function () {
+        home.style.backgroundColor = document.querySelector('#color-home').value;
+    })
 
-// portfolio
-document.querySelector('#color-portfolio').addEventListener('input', function () {
-    portfolio.style.backgroundColor = document.querySelector('#color-portfolio').value;
-})
+    // about
+    document.querySelector('#color-about').addEventListener('input', function () {
+        about.style.backgroundColor = document.querySelector('#color-about').value;
+    })
 
-// contact US 
-document.querySelector('#color-contact').addEventListener('input', function () {
-    contactUs.style.background = document.querySelector('#color-contact').value;
-})
+    // my project
+    document.querySelector('#color-project').addEventListener('input', function () {
+        myProject.style.background = document.querySelector('#color-project').value;
+    })
 
-const clearAll = () => {
-    home.style.backgroundColor = '#003049';
-    about.style.backgroundColor = '#040404';
-    myProject.style.background = 'linear-gradient(45deg, #08001f, #30197d)';
-    portfolio.style.backgroundColor = '#003049';
-    contactUs.style.background = '#2c2f48';
-    document.querySelector('#color-home').value = '';
-    document.querySelector('#color-about').value = '';
-    document.querySelector('#color-project').value = '';
-    document.querySelector('#color-portfolio').value = '';
-    document.querySelector('#color-contact').value = '';
-}
+    // portfolio
+    document.querySelector('#color-portfolio').addEventListener('input', function () {
+        portfolio.style.backgroundColor = document.querySelector('#color-portfolio').value;
+    })
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Saytingiz yuklandi
-    var preloader = document.querySelector(".loader");
+    // contact US 
+    document.querySelector('#color-contact').addEventListener('input', function () {
+        contactUs.style.background = document.querySelector('#color-contact').value;
+    })
 
-    // 3 sekunddan so'ng "preloader"ni yashirish
+    const clearAll = () => {
+        home.style.backgroundColor = '#003049';
+        about.style.backgroundColor = '#040404';
+        myProject.style.background = 'linear-gradient(45deg, #08001f, #30197d)';
+        portfolio.style.backgroundColor = '#003049';
+        contactUs.style.background = '#2c2f48';
+        document.querySelector('#color-home').value = '';
+        document.querySelector('#color-about').value = '';
+        document.querySelector('#color-project').value = '';
+        document.querySelector('#color-portfolio').value = '';
+        document.querySelector('#color-contact').value = '';
+    }
+
+    document.addEventListener("DOMContentLoaded", function () {
+        // Saytingiz yuklandi
+        var preloader = document.querySelector(".loader");
+
+        // 3 sekunddan so'ng "preloader"ni yashirish
+        setTimeout(function () {
+            preloader.style.display = "none";
+        }, 1200);
+    });
+
     setTimeout(function () {
-        preloader.style.display = "none";
-    }, 1200);
-});
-
-setTimeout(function () {
-    let homeImg = document.querySelector('.home_rasm-col');
-    homeImg.classList.add('scaleImage');
-}, 2000);
+        let homeImg = document.querySelector('.home_rasm-col');
+        homeImg.classList.add('scaleImage');
+    }, 2000);
+})
